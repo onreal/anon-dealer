0828d149a4dcc38b416c81653b7f75cda1dbe296
Margarit Koka <panagiotis.kokas@gmail.com>
2025-09-09 19:28:30 +0300

    Feat: extend configuration model for P2P settings

--- src/persistent/model/Configuration.ts
+++ src/persistent/model/Configuration.ts
@@ -6,6 +6,11 @@ interface Configuration {
     IsBackend: boolean
     ServerUrl: string
     ServerToken: string
+    // P2P Signaling Server Configuration
+    IsP2PEnabled: boolean
+    SignalingServerUrl: string
+    SignalingServerWsUrl: string
+    P2PDefaultRoom: string
     CreatedOn: Date
     LastLogin: Date
 }
